b984e95f0ef02ad41ae973ee55d755624da13e1c
wonjinkim111 <diffrentstar@naver.com>
2025-04-30 20:56:31 +0900

    250430 Modified signin.js v2

--- src/components/signIn.js
+++ src/components/signIn.js
@@ -82,29 +82,15 @@ export default function SignIn(props){
     // })
 
     //axios없을때 테스트용
-    //const [test] = React.useState({
-    //  name: 'test',
-    //  id: 'test',
-    //  token: 'testtoken',
-    //  email: 'test@codeReview.com',
-    //  encryptedPassword: 'test1234',
-    //  gender: 0,
-    //  regDate: '2020-05-30 00:00:00'
-//  })
-    .then(response => {
-      console.log(response.data); // 예: {userId: 5, token: "...", mentorId: 10, menteeId: 4}
-    
-      sessionStorage.setItem('user', JSON.stringify({
-        id: response.data.userId,
-        token: response.data.token,
-        mentorId: response.data.mentorId,
-        menteeId: response.data.menteeId
-      }));
-    
-      window.location.href = '/';  // 로그인 성공 시 홈으로 이동
-    })
-    
-
+    const [test] = React.useState({
+      name: 'test',
+      id: 'test',
+      token: 'testtoken',
+      email: 'test@codeReview.com',
+      encryptedPassword: 'test1234',
+      gender: 0,
+      regDate: '2020-05-30 00:00:00'
+  })
 
     const handleChangeForm = e => {
       setValues({...values, [e.target.name]: e.target.value});
@@ -140,12 +126,21 @@ export default function SignIn(props){
         email: values.email,
         encryptedPassword: values.encryptedPassword
       })
-       .then(response =>{console.log(response)
-        sessionStorage.setItem('user', JSON.stringify({id:response.headers.userid,token: response.headers.token, mentorid: response.headers.mentorid, menteeid: response.headers.menteeid}))
-        window.location.href='/';})
-        .catch(error => {
-          alert("틀렸습니다.")
-          setValues({email:'', encryptedPassword:''});
+       //.then(response =>{console.log(response)
+       // sessionStorage.setItem('user', JSON.stringify({id:response.headers.userid,token: response.headers.token, mentorid: response.headers.mentorid, menteeid: response.headers.menteeid}))
+       // window.location.href='/';})
+       // .catch(error => {
+       //   alert("틀렸습니다.")
+       //   setValues({email:'', encryptedPassword:''});
+       .then(response => {
+        console.log(response.data);
+        sessionStorage.setItem('user', JSON.stringify({
+          id: response.data.userId,
+          token: response.data.token,
+          mentorId: response.data.mentorId,
+          menteeId: response.data.menteeId
+        }));
+        window.location.href = '/';
         })
       
     }
