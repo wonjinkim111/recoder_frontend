bf12bf31354942b67b0d868ba4f25c24c59a1da8
wonjinkim111 <diffrentstar@naver.com>
2025-07-10 20:32:26 +0900

    250710_Modified IP

--- src/modules/menteeDashboard2.js
+++ src/modules/menteeDashboard2.js
@@ -27,7 +27,7 @@ export default function MentorDashBoard(props){
 
   // useEffect(() => {
   //   const userData = JSON.parse(sessionStorage.getItem('user'));
-  //   const url = `http://192.168.45.20145.207:10000/users/${userData.id}`;
+  //   const url = `http://192.168.45.13545.207:10000/users/${userData.id}`;
   //   axios.get(url)
   //   .then(response =>{
   //     console.log(response);
@@ -54,7 +54,7 @@ export default function MentorDashBoard(props){
   }
   console.log("✅ userData", userData);
 
-  const url = `http://192.168.45.201:32513/users/${userData.id}`;
+  const url = `http://192.168.45.135:32513/users/${userData.id}`;
   axios.get(url)
     .then(response => {
       console.log("✅ API 응답:", response.data);
